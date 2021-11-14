0f65faca840e57ea5391801064f04bcce5c145ce
lauretteroy <laurieroy.dev@gmail.com>
2021-11-13 20:06:08 -0600

    Fix typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_person_name
   has_many :shots, dependent: :destroy
-  has_many :commentss, dependent: :destroy
+  has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
